bd4919fc60c22edc7f449de96388625854162e4f
Nitesh Bawane <nitesh.bawane1@gmail.com>
2018-05-31 13:54:00 +0530

    Domain extraction and URL listing done

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from tweepy.streaming import StreamListener
 import sqlite3
 import json
 import threading
-
+import time
+import requests
+from urllib.parse import urlparse
 
 consumer_key = 'NiBKgW5YIu2TKm3kJE4RZKRXz'
 consumer_secret = '8ZPCDCTREa6DF4tm7NpInP1iDq9a7bkPIBQRVyH2Q1cBlLqPHo'
@@ -16,24 +18,35 @@ class listener(StreamListener):
 	def __init__(self):
 		threading.Timer(15, self.generate_report).start()
 		self.conn = sqlite3.connect('twitter.db')    #initialize db
+
 		self.dbobj = self.conn.cursor()
+
 		try:
 			self.dbobj.execute('''CREATE TABLE tweets
-             (user text, time text, urls text)''')
+             (user text,url text, time bigint)''')
+
 		except sqlite3.OperationalError as e:
 			print('Table already present, no need to create')
+		try:
 
+			self.dbobj.execute('''CREATE TABLE urls
+			 (url text, time bigint)''')
+		except sqlite3.OperationalError as e:
+			print('Table already present, no need to create')
 
 	def on_data(self, data):
 		data = json.loads(data)
 		user = data['user']['name']
-		tweet_time = data['created_at']
-		urls_list = data["entities"]["urls"]
+		tweet_time = int(time.time())	#timestamp when twwet recieved by programm to be changed to created_at
+		self.urls_in_tweets = data["entities"]["urls"]
 		final_url = ''
-		for url_element in urls_list:
+		for url_element in self.urls_in_tweets:
 			url_append = url_element["expanded_url"]
 			if not('twitter' in url_append and 'status' in url_append):  #twitter statuses to ignored
-				final_url += url_append
+				final_url += url_append+','
+				self.dbobj.execute("insert into urls values(?,?)",(url_append,tweet_time))
+		if len(final_url)==0:
+			final_url = None
 		self.dbobj.execute("insert into tweets values(?,?,?)",(user,tweet_time,final_url))
 
 		# print(user)
@@ -51,9 +64,57 @@ class listener(StreamListener):
 		threading.Timer(15,self.generate_report).start()	#self calling logic, call after 1 min
 		self.process = sqlite3.connect('twitter.db')    #initialize db
 		self.processdbobj = self.process.cursor()
+		self.urls = []
+		thread_objs = []
+		domain_list = []
+		domain_dict = {}
+		# self.processurl = sqlite3.connect('urls.db')
+		# self.processurldbobj = self.processurl.cursor()
 		print("#"*100)
-		for i in self.processdbobj.execute("select * from tweets"):
+
+		#Get Users from last 5 mins
+		for i in self.processdbobj.execute("select user,count(user) from tweets where time>? group by user",(time.time()-300,)):	#should give data for last 5 mins, 5*60
 			print(i)
+		#Get total numbers of urls
+		url_count =  self.processdbobj.execute("select count(url) from urls where time>?",((time.time()-300),))
+		print("Total number of URLS mentioned ::  {}".format(url_count.fetchone()))
+
+		#Extract domain Info
+		for i in self.processdbobj.execute("select url from urls where time>?", ((time.time() - 300),)):	#accumulating urls
+			self.urls.append(i[0])
+
+		for url_append in self.urls:
+			if not ('twitter' in url_append and 'status' in url_append):  # twitter statuses to ignored
+				tobj = threading.Thread(target=self.unshorten_url, args=(url_append,))
+				thread_objs.append(tobj)
+				tobj.start()
+		for obj in thread_objs:
+			obj.join()
+
+		for link in self.urls:
+			print('%s' % link)
+			urlobj = urlparse(link)
+			domain_list.append(urlobj.netloc)
+
+		for domain in domain_list:
+			if domain not in domain_dict:
+				domain_dict[domain] = 1
+			else:
+				domain_dict[domain] = domain_dict[domain] + 1
+		ascending_order_domains_usage = sorted(domain_dict.items(), key=lambda elem: -elem[1])
+		print('-' * 100)
+		print('\n')
+		print('Unique domains and number of times the domain used::::::\n')
+		for domain in ascending_order_domains_usage:
+
+			print('{}  :  {}'.format(domain[0], domain[1]))
+		#Extract domain Info finished
+	def unshorten_url(self,url):
+		#shorten the expanded URLs
+		try:
+			self.urls.append(requests.head(url, allow_redirects=True).url)
+		except requests.exceptions.ConnectionError as e:
+			print("Connection Error, Check connection")
 
 
 
@@ -61,4 +122,4 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(Access_Token, Access_Token_Secret)
 
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["india"],languages=["en"])
+twitterStream.filter(track=["bitcoin"],languages=["en"])
